ffa4c3e29e8a04ccab5994eaa540fd3ae65f649b
Pietrell <piotrplotast2006@gmail.com>
2024-12-05 19:49:39 +0100

    move file and add height

--- src/AppMain.tsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <h1>
-      <span>Test</span>
-    </h1>
-  );
-}
-
-export default App;
---
+++ src/components/AppMain.tsx
@@ -0,0 +1,10 @@
+import SkillList from "./SkillList";
+function App() {
+  return (
+    <div className="h-[100dvh]">
+      <SkillList />
+    </div>
+  );
+}
+
+export default App;
