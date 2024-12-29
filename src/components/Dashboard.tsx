f879f839ccf97f368c47e8161eb7996d7e1573c0
Pietrell <piotrplotast2006@gmail.com>
2024-12-29 16:06:52 +0100

    add Dashboard component to display total time and random quotes

---
+++ src/components/Dashboard.tsx
@@ -0,0 +1,24 @@
+import { useState, useEffect } from "react";
+import quotes from "../quotes.json";
+export default function Dashboard({ totalTime }: { totalTime: number }) {
+  const [randomQuote, setRandomQuote] = useState(
+    "Eighty percent of success is showing up."
+  );
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      const newQoute =
+        quotes.quotes[Math.floor(Math.random() * quotes.quotes.length)];
+      setRandomQuote(newQoute.quote);
+    }, 5000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return (
+    <div>
+      <h2>Time spent tracking your skills: {totalTime}</h2>
+      <h3>Charts</h3>
+      <h4>{randomQuote}</h4>
+    </div>
+  );
+}
