fe0433459d0918c04a95380c8b8b2d5f94192cf2
Pietrell <piotrplotast2006@gmail.com>
2024-12-29 16:07:11 +0100

    refactor Skill components to use time tracking and update UI elements

--- src/components/Skill.tsx
+++ src/components/Skill.tsx
@@ -3,19 +3,17 @@ import { useState, useRef, useEffect } from "react";
 interface SkillProps {
   skillName: string;
   level: number;
-  progress: number;
+  time: number;
   icon: string;
   onHandleProgress: (skill: string) => void;
-  onHandleLvlUp: (skill: string) => void;
 }
 
 export default function Skill({
   skillName,
   level,
-  progress,
+  time,
   icon,
   onHandleProgress,
-  onHandleLvlUp,
 }: SkillProps) {
   const [isStarted, setIsStarted] = useState(false);
 
@@ -30,9 +28,6 @@ export default function Skill({
       intervalIdRef.current = setInterval(() => {
         onHandleProgress(skillName);
       }, 1000);
-      if (progress >= 100) {
-        onHandleLvlUp(skillName);
-      }
     } else if (intervalIdRef.current) {
       clearInterval(intervalIdRef.current);
       intervalIdRef.current = null;
@@ -49,12 +44,7 @@ export default function Skill({
     <div className="flex w-48">
       <h2>{icon}</h2>
       <h2>{skillName}</h2>
-      <h2 className="w-16">
-        <span
-          className={`h-full block bg-green-300`}
-          style={{ width: `calc(100% - calc(100% - ${progress}%))` }}
-        ></span>
-      </h2>
+      <h2>{time}</h2>
       <h2>{level}</h2>
       <button onClick={handleProgress}>{isStarted ? "Stop" : "Start"}</button>
     </div>
