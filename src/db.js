9e5aaa007a0cca72d4053d541f3a5af9865431e7
Pietrell <piotrplotast2006@gmail.com>
2024-12-29 16:03:38 +0100

    add db table fot dashboard data and clear code

--- src/db.js
+++ src/db.js
@@ -1,5 +1,4 @@
 import sqlite3 from "sqlite3";
-console.log("Initializing database...");
 const db = new sqlite3.Database("db.sqlite", (err) => {
   if (err) {
     console.error("Error opening database", err);
@@ -8,14 +7,35 @@ const db = new sqlite3.Database("db.sqlite", (err) => {
   }
 });
 db.run(
-  "CREATE TABLE IF NOT EXISTS skills (id INTEGER PRIMARY KEY, name TEXT, icon TEXT, level INTEGER, progress INTEGER)",
+  "CREATE TABLE IF NOT EXISTS skills (id INTEGER PRIMARY KEY, name TEXT, icon TEXT, time INTEGER)",
   (err) => {
     if (err) {
       console.error("Error creating table", err);
     } else {
-      console.log("Table created successfully");
+      console.log("Table skills created successfully");
     }
   }
 );
-console.log("Database initialization script executed.");
+db.run(
+  "CREATE TABLE IF NOT EXISTS dashboard_data (id INTEGER PRIMARY KEY, total_skills INTEGER, time_today INTEGER, total_time INTEGER)",
+  (err) => {
+    if (err) {
+      console.error("Error creating table", err);
+    } else {
+      console.log("Table dashboard_data created successfully");
+      db.run(
+        "INSERT INTO dashboard_data (total_skills, time_today, total_time) VALUES (0, 0, 0)",
+        (err) => {
+          if (err) {
+            console.error("Error inserting initial data", err);
+          } else {
+            console.log(
+              "Initial data inserted into dashboard_data successfully"
+            );
+          }
+        }
+      );
+    }
+  }
+);
 export default db;
