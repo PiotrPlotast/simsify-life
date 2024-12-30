40d4a7472f0b43618d7491b355a97431219b18fc
Pietrell <piotrplotast2006@gmail.com>
2024-12-30 19:25:23 +0100

    clear console logs

--- src/db.js
+++ src/db.js
@@ -2,8 +2,6 @@ import sqlite3 from "sqlite3";
 const db = new sqlite3.Database("db.sqlite", (err) => {
   if (err) {
     console.error("Error opening database", err);
-  } else {
-    console.log("Database opened successfully");
   }
 });
 db.run(
@@ -11,8 +9,6 @@ db.run(
   (err) => {
     if (err) {
       console.error("Error creating table", err);
-    } else {
-      console.log("Table skills created successfully");
     }
   }
 );
@@ -22,16 +18,11 @@ db.run(
     if (err) {
       console.error("Error creating table", err);
     } else {
-      console.log("Table dashboard_data created successfully");
       db.run(
         "INSERT INTO dashboard_data (total_skills, time_today, total_time) VALUES (0, 0, 0)",
         (err) => {
           if (err) {
             console.error("Error inserting initial data", err);
-          } else {
-            console.log(
-              "Initial data inserted into dashboard_data successfully"
-            );
           }
         }
       );
