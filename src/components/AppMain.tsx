aabb2bb51976461ca417959553d2deb66b1819fc
Pietrell <piotrplotast2006@gmail.com>
2025-01-01 19:31:11 +0100

    feat: implement dark mode toggle and settings popup in main app component

--- src/components/AppMain.tsx
+++ src/components/AppMain.tsx
@@ -1,8 +1,11 @@
 import { useState, useEffect } from "react";
 import SkillList from "./SkillList";
 import Dashboard from "./Dashboard";
+import SettingsPopUp from "./SettingsPopUp";
 function App() {
   const [totalTime, setTotalTime] = useState(0);
+  const [isSettingsOpen, setIsSettingsOpen] = useState(false);
+  const [isDarkMode, setIsDarkMode] = useState(0);
 
   useEffect(() => {
     window.Electron.ipcRenderer
@@ -19,17 +22,51 @@ function App() {
       });
   }, []);
 
+  useEffect(() => {
+    window.Electron.ipcRenderer
+      .invoke("get-dark-mode")
+      .then((row) => {
+        setIsDarkMode(row.dark_mode);
+      })
+      .catch((err) => {
+        console.error("Error querying dark mode", err);
+      });
+  }, []);
+
+  useEffect(() => {
+    if (isDarkMode === 1) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [isDarkMode]);
+
   function handleSetTotalTime() {
     window.Electron.ipcRenderer
       .invoke("set-total-time", totalTime + 1)
       .then(() => setTotalTime(totalTime + 1));
   }
+
+  function handleSetDarkMode() {
+    window.Electron.ipcRenderer
+      .invoke("set-dark-mode", isDarkMode === 1 ? 0 : 1)
+      .then(() => setIsDarkMode(isDarkMode === 1 ? 0 : 1));
+  }
   return (
-    <div className="h-[100dvh] flex flex-col items-center">
-      <h1 className="text-2xl font-bold">Simsify Life</h1>
+    <div className="h-[100dvh] w-[100dvw] flex flex-col items-center pt-8">
+      <h1 className="text-2xl font-bold">Skills Time Tracker</h1>
       <Dashboard totalTime={totalTime} />
       <SkillList onSetTotalTime={handleSetTotalTime} />
-      <button className="text-5xl fixed bottom-0 right-0 p-8">⚙</button>
+      <SettingsPopUp
+        onSetHandleDarkMode={handleSetDarkMode}
+        isSettingsOpen={isSettingsOpen}
+      />
+      <button
+        onClick={() => setIsSettingsOpen(!isSettingsOpen)}
+        className="text-5xl fixed bottom-0 right-0 p-8"
+      >
+        ⚙
+      </button>
     </div>
   );
 }
